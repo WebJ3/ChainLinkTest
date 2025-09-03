ca6631d1526f55d7b57cdba7bf339bb11fd366fa
duruo850 <zhanchenjin.2008@163.com>
2025-09-03 16:40:54 +0800

    + add log

--- docs.chain.link/samples/DataFeeds/EnsConsumer-Sepolia.sol
+++ docs.chain.link/samples/DataFeeds/EnsConsumer-Sepolia.sol
@@ -34,6 +34,7 @@ contract ENSConsumer {
         return resolver.addr(node);
     }
 
+    // 'eth-usd.data.eth'域名绑定0x10E7e7D64d7dA687f7DcF8443Df58A0415329b15合约地址
     // 'eth-usd.data.eth'对应的hash是0xf599f4cd075a34b92169cf57271da65a7a936c35e3f31e854447fbb3e7eb736d
     // 这个哈希就是通过namehash算法计算得到的。但是，合约内部不能直接从一个字符串计算出namehash，因为namehash算法需要递归地进行哈希，这在Solidity中计算成本很高（而且需要处理字符串和递归）。
     // 因此，通常我们在链下计算好namehash，然后将哈希值作为参数传递给合约函数
